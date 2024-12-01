b50f1d2ba0b47ad9e4aaa80ad22b93bff2f7eb90
Nyot-Nyot <dzakisultan012@gmail.com>
2024-12-01 19:35:58 +0700

    Initialize chat application with basic Client and Server classes, Maven configuration, and unit tests

---
+++ src/main/java/com/chat/Client.java
@@ -0,0 +1,9 @@
+package com.chat;
+
+public class Client {
+    // minta server ip dan username
+    // buka socket
+    // buat thread untuk menerima pesan dari server
+    // buat method untuk menulis pesan ke server
+    // tutup socket
+}
---
+++ src/main/java/com/chat/Server.java
@@ -0,0 +1,13 @@
+package com.chat;
+
+public class Server {
+    // buka serversocket
+    // buat data structure untuk menyimpan clients
+    // tampilkan ip address server
+    // terus terima client di thread baru
+    // jika terdapat client maka tambahkan ke struktur data
+    // jika terdapat pesan dari client, kirim ke semua client yg terhubung
+    // jika client keluar:
+    // hapus client dari list
+    // putuskan socket nya
+}
