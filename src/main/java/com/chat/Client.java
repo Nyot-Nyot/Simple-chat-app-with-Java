87079b9c00702e9c2c66268515a0afde1200f4c6
Nyot-Nyot <dzakisultan012@gmail.com>
2024-12-01 20:39:29 +0700

    Remove commented-out code and clean up Client class

--- src/main/java/com/chat/Client.java
+++ src/main/java/com/chat/Client.java
@@ -39,11 +39,4 @@ public class Client {
             }
         }
     }
-
-
-    // minta server ip dan username
-    // buka socket
-    // buat thread untuk menerima pesan dari server
-    // buat method untuk menulis pesan ke server
-    // tutup socket
 }
